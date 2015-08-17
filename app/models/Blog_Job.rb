18dff37ff3c98b25d826f2f81590ccacdf0df737
Alex Parayannilam <alexjpar@gmail.com>
2015-08-17 19:18:41 -0400

    Added progress updates to perform method. to be used in ajax polling

--- app/models/Blog_Job.rb
+++ app/models/Blog_Job.rb
@@ -8,6 +8,7 @@ class BlogJob < Struct.new(:id, :post_type, :search_query, :show_reblogs, :numbe
     	posts = Array.new
 	    i = 0
 	    blog = Blog.find(id)
+	    blog.update(progress: 0)
     	until i > numberOfPosts
     		#Increment by 20 bc need to increase offset for request. only 20 posts returned at a time.
     		if (post_type == "all")
@@ -16,6 +17,7 @@ class BlogJob < Struct.new(:id, :post_type, :search_query, :show_reblogs, :numbe
 	    	#Don't need reblog info (used to remove reblogs from array) if the user wants to see reblogs.
 	    		posts += (myClient.posts(search_query, :type => post_type, "reblog_info" => !show_reblogs, "offset" => i ) )["posts"]
 	    	end
+	    	blog.update(progress: i/numberOfPosts)
 	    	i = i + 20
     	end
    
@@ -24,7 +26,6 @@ class BlogJob < Struct.new(:id, :post_type, :search_query, :show_reblogs, :numbe
 			posts = posts.select {|x| x["reblogged_from_id"] == nil}
 		end
 		#Sort by descending note count, and return first 30
-		blog.posts = posts.sort_by {|x| x["note_count"]}.reverse![0,30]
-		blog.save
+		blog.update(posts: posts.sort_by {|x| x["note_count"]}.reverse![0,30], progress: 100)
 	end
 end
