b816df02b9728eb0a920094a95172d45a637dfb1
Alex Parayannilam <alexjpar@gmail.com>
2015-08-26 01:47:03 -0400

    Added a post renderer helper to remove logic from blog partial

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,45 @@
 module BlogsHelper
+	def post_renderer(post)
+		post_type = post["type"]
+		case post_type
+		when 'photo'
+			short_url_block(post){ content_tag :img, nil, src: post["photos"][0]["alt_sizes"][2]["url"] }
+		when 'text'
+			if post["title"].present? 
+				short_url_block(post){ post["title"] }
+			else
+				short_url_block(post){"link to text post"}
+			end
+		when 'video'
+			if post["thumbnail_url"].present?
+	  			short_url_block(post){ content_tag :img, nil, src: post["thumbnail_url"] }
+			else
+				short_url_block(post){ "link to video post" }
+	  		end
+		when 'audio'
+			if post["thumbnail_url"].present?
+	  			short_url_block(post){ content_tag :img, nil, src: post["album_art"] }
+			else
+				short_url_block(post){ "link to audio post" }
+	  		end
+		when 'quote'
+			short_url_block(post){ post["text"] }
+		when 'link'
+			short_url_block(post){ post["title"] }
+		when 'chat'
+			if post["title"].present?
+				short_url_block(post){ content_tag :h5, post["title"] }
+			else
+				short_url_block(post){ "link to chat post" }
+			end
+		when 'answer'
+			short_url_block(post){ content_tag :h5, post["question"] }
+		end
+	end
+
+	def short_url_block (post)
+		content_tag :a, href: post["short_url"], target: "_blank" do 
+			yield
+		end
+	end
 end
