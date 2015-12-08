f26cdebe0d6aaeec3ced9f0f49e991eb56b49121
Alex Parayannilam <alexjpar@gmail.com>
2015-12-07 22:41:43 -0500

    Retrieving posts from a blog is much quicker now because multiple threads are spawned to retrieve posts simultaneously

--- app/jobs/Blog_Job.rb
+++ app/jobs/Blog_Job.rb
@@ -1,11 +1,14 @@
-class BlogJob < ProgressJob::Base 
+class BlogJob < ProgressJob::Base
+
+  #Number of threads used to send requests to API
+  THREAD_COUNT = 5
   #Parameters are id of blog and total number of posts for blog
   def initialize(id, number_of_posts)
     super progress_max: number_of_posts
     @id = id
     @number_of_posts = number_of_posts
   end
-
+  
   def perform 
     #initialize blog variables
     blog = Blog.find(@id)
@@ -16,29 +19,25 @@ class BlogJob < ProgressJob::Base
     # Create new client to make requests to Tumblr API
     myClient = Tumblr::Client.new
     
-    
-    update_stage ("Searching " + 0.to_s + "/" + @number_of_posts.to_s + " blog posts...")
+    update_stage ("Searching blog posts...")
 
     #final post array
     posts = Array.new
 
     post_types.each do |post_type|
-      #temp variables for loop
-      offset = 0
-      temp_posts = Array.new
-        loop do
-          
-          temp_posts = (myClient.posts(search_query, :type => post_type, "reblog_info" => !show_reblogs, "offset" => offset ) )["posts"]
-          
-          #Need to increment offset by 20 for next request.
-          offset = offset + 20
-          update_progress(step: temp_posts.length)
-          update_stage (posts.length.to_s + "/" + @number_of_posts.to_s + " blog posts processed")
-          
-          #add the fetched posts to the final post array, break if no posts were received
-          break if temp_posts.blank?
-          posts += temp_posts
-        end
+      counter = 0
+      threads = []
+      threads  = (1..THREAD_COUNT).collect do |k|
+        offset = counter
+        t = Thread.new{getPosts(myClient, search_query, post_type, show_reblogs, offset)}
+        counter += 20
+        t
+      end
+
+      threads.each do |t|
+        t.join
+        posts += t.value
+      end
     end
     #Ignore reblogs if user doesn't want reblogs
     if !@show_reblogs
@@ -50,6 +49,23 @@ class BlogJob < ProgressJob::Base
     blog.update(posts: posts.delete_if {|x| x["note_count"].nil? || x["note_count"] == 0 }.sort_by {|x| x["note_count"] }.reverse![0,30])
 
     update_progress_max(100)
+  end
+
+  def getPosts(myClient, search_query, post_type, show_reblogs, offset)
+    posts = Array.new
+    loop do
+        temp_posts = Array.new
+        temp_posts = (myClient.posts(search_query, :type => post_type, "reblog_info" => !show_reblogs, "offset" => offset ) )["posts"]
+        
+        #Need to increment offset by 80 for next request bc 4 threads
+        offset = offset + (THREAD_COUNT * 20)
+        update_progress(step: temp_posts.length)
+        
+        #add the fetched posts to the final post array, break if no posts were received
+        break if temp_posts.blank?
+        posts += temp_posts
+    end
+    return posts
   end
 
   def max_run_time
